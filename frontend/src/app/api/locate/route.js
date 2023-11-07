112f864ad6b2e294ad0f209622c1c6daeb897d97
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2023-11-07 11:07:20 +0900

    connect mine

---
+++ frontend/src/app/api/locate/route.js
@@ -0,0 +1,48 @@
+// app/api/location/route.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function GET() {
+    const { data, error } = await supabase
+        .from('locations')
+        .select('*');
+
+    if (error) {
+        console.error('Error fetching data: ', error);
+        return new Response(JSON.stringify({ error: 'Error fetching data' }), {
+            status: 500,
+        });
+    }
+
+    return new Response(JSON.stringify(data), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
+export async function POST(request) {
+    const { user_id, latitude, longitude } = await request.json();
+    const { data, error } = await supabase
+        .from('locations')
+        .insert([
+            { user_id, latitude, longitude }
+        ]);
+
+    if (error) {
+        console.error('Error posting data: ', error);
+        return new Response(JSON.stringify({ error: 'Error posting data' }), {
+            status: 500,
+        });
+    }
+
+    return new Response(JSON.stringify(data), {
+        status: 201,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
