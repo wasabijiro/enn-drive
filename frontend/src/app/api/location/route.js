eaa161e71415664f91602672bf350f3744643831
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2023-11-09 17:34:44 +0900

    apikey to un NEXT=PUBLIC

--- frontend/src/app/api/location/route.js
+++ frontend/src/app/api/location/route.js
@@ -1,8 +1,8 @@
 // app/api/location/route.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function GET() {
