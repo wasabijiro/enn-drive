b6ef3144f6f70e4109afc1ec33c8df9dc115a029
wasabijiro <shushu011023@gmail.com>
2023-11-15 01:24:58 +0900

    update packege id

--- frontend/src/libs/txb.ts
+++ frontend/src/libs/txb.ts
@@ -4,30 +4,19 @@ import {
 } from "@mysten/sui.js/transactions";
 
 export const PACKAGE_ID =
-  "0xdbda1f10ee21cea9f017a6c103b144166138966e5c09a3d9f43e4d9f03b16c2e";
+  "0x0e0131a7ce2b0d4090652f1a666ddec8ed17b933af9f7f03d8d2b2ae642f6ff7";
 
-export const EVENT_CONFIG_ID =
-  "0x1a98d7905b3ab0d9ef374f548dd7ab947b304dd5f9083ff9eb8adf0d698f0688";
+export const NFT_INDEX_ID =
+  "0x461eedd2255074df76b4a0cd92ed7d1702ddd023f6c1e1b8c871c06bae20c6ae";
 
 export const CLOCK_ID = "0x6";
 
-export const EVENT_KEY = "movejp10";
-
-export interface MintArgs {
-  // list: string | TransactionArgument;
-  event_key: string | TransactionArgument;
-  name: string | TransactionArgument;
-  description: string | TransactionArgument;
-  url: string | TransactionArgument;
-}
-
-export function firstMint(txb: TransactionBlock, args: MintArgs) {
+export function callMint(txb: TransactionBlock, args: any) {
   return txb.moveCall({
     target: `${PACKAGE_ID}::issuer::mint`,
     arguments: [
-      txb.pure(EVENT_CONFIG_ID),
+      txb.pure(NFT_INDEX_ID),
       txb.pure(CLOCK_ID),
-      txb.pure(EVENT_KEY),
       txb.pure(args.name),
       txb.pure(args.description),
       txb.pure(args.url),
@@ -38,14 +27,12 @@ export function firstMint(txb: TransactionBlock, args: MintArgs) {
 export const moveCallMintNft = (
   txb: TransactionBlock,
   props: {
-    event_key: string;
     name: string;
     description: string;
     url: string;
   }
 ) => {
-  firstMint(txb, {
-    event_key: props.event_key,
+  callMint(txb, {
     name: props.name,
     description: props.description,
     url: props.url,
