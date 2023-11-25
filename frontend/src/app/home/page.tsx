81be5a5ebbe172714f32c0a7bfbf11dcee53bdb7
wasabijiro <shushu011023@gmail.com>
2023-11-25 11:30:21 +0900

    fix routing

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -116,7 +116,7 @@ export default function Home() {
                 setErr("Double Mint rejected...");
                 throw new Error("objectType not found");
               }
-              router.push(`/`);
+              router.push(`/like`);
             } else {
               // setErr(`Transaction Failed: ${result.effects?.status.error}`);
               setErr("Transaction Failed...");
